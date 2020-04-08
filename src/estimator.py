fd67f1693c5441c9e38bb8755e4af418734bcb78
The 254-kid <mgabbana8@yahoo.com>
2020-04-08 17:06:54 +0300

    Trial 4.3

--- src/estimator.py
+++ src/estimator.py
@@ -1,52 +1,54 @@
 def estimator(data):
+	input = data
 
-	periodType = data.periodType
-	timeToElapse = data.timeToElapse
-	reportedCases = data.reportedCases
+	periodType = input.periodType
+	timeToElapse = input.timeToElapse
+	reportedCases = input.reportedCases
 
 	factor = factor_calculator(periodType, timeToElapse)
 
-	impact(reportedCases, factor)
-	severeImpact(reportedCases, factor)
+	impact = impact(reportedCases, factor)
+	severe= severeImpact(reportedCases, factor)
 
-	return {
+	newdata = {
 
-	data:{data},
+	data: input,
 	impact:{
-		currentlyInfected : currently_infected_impact,
-		InfectionsByRequestedTime : infectionsByRequestedTime_impact
+		currentlyInfected : impact[0],
+		InfectionsByRequestedTime : impact[1]
 		},
 	severeImpact:{
-		currentlyInfected :currently_infected_severe,
-		infectionsByRequestedTime : infectionsByRequestedTime_severe
+		currentlyInfected :severe[0],
+		infectionsByRequestedTime : severe[1]
 	}
 
 	}
+	data = newdata
+
+	return data
 
 
-	def impact(reportedCases, factor):
-		global currently_infected_impact
-		global infectionsByRequestedTime_impact
-		currently_infected_impact = reportedCases * 10
-		infectionsByRequestedTime_impact = currently_infected *(2**factor)
+def impact(reportedCases, factor):
+	currently_infected_impact = reportedCases * 10
+	infectionsByRequestedTime_impact = currently_infected *(2**factor)
+	return [currently_infected_impact, infectionsByRequestedTime_impact]
 		
 
-	def severeImpact(reportedCases, factor):
-		global currently_infected_severe
-		global infectionsByRequestedTime_severe
-		currently_infected_severe = reportedCases * 50
-		infectionsByRequestedTime_severe = currently_infected *(2**factor)
+def severeImpact(reportedCases, factor):
+	currently_infected_severe = reportedCases * 50
+	infectionsByRequestedTime_severe = currently_infected *(2**factor)
+	return [currently_infected_severe, infectionsByRequestedTime_severe]
 	
 		
 	#Function to calculate and return the factor with all period type variations
-	def factor_calculator(periodType, timeToElapse):
-		if periodType == "days":
-			days = timeToElapse
-		elif periodType == "weeks":
-			days = 7 * timeToElapse
-		elif periodType == "months":
-			days = 30 * timeToElapse
+def factor_calculator(periodType, timeToElapse):
+	if periodType == "days":
+		days = timeToElapse
+	elif periodType == "weeks":
+		days = 7 * timeToElapse
+	elif periodType == "months":
+		days = 30 * timeToElapse
 
-		factor = days//3
+	factor = days//3
 
-		return factor
+	return factor
