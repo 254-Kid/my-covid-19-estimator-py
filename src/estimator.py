c80cd9fbe6d0c87c56e7ef67096f4feeb582e068
The 254-kid <mgabbana8@yahoo.com>
2020-04-08 14:55:39 +0300

    Third one

--- src/estimator.py
+++ src/estimator.py
@@ -1,51 +1,56 @@
-finaldata = {
-
-	"data":{},
-	"impact":{},
-	"severeImpact":{}
-
-	}
-
 def estimator(data):
-	newdata = data
-	periodType = newdata["periodType"]
-	timeToElapse = newdata["timeToElapse"]
-	reportedCases = newdata["reportedCases"]
+
+	periodType = data.periodType
+	timeToElapse = data.timeToElapse
+	reportedCases = data.reportedCases
+
 	factor = factor_calculator(periodType, timeToElapse)
 
 	impact(reportedCases, factor)
 	severeImpact(reportedCases, factor)
 
-	finaldata["data"] = data
 
-	data = finaldata
-	return data
 
-def impact(reportedCases, factor):
-	currently_infected = reportedCases * 10
-	InfectionsByRequestedTime = currently_infected *(2**factor)
-	finaldata["impact"]["currently_infected"] = currently_infected
-	finaldata["impact"]["InfectionsByRequestedTime"] = InfectionsByRequestedTime
-	return
+	def impact(reportedCases, factor):
+		global currently_infected_impact
+		global InfectionsByRequestedTime_impact
+		currently_infected_impact = reportedCases * 10
+		InfectionsByRequestedTime_impact = currently_infected *(2**factor)
+		
 
+	def severeImpact(reportedCases, factor):
+		global currently_infected_severeImpact
+		global InfectionsByRequestedTime_severeImpact
+		currently_infected_severeImpact = reportedCases * 50
+		InfectionsByRequestedTime_severeImpact = currently_infected *(2**factor)
 	
+		
+	#Function to calculate and return the factor with all period type variations
+	def factor_calculator(periodType, timeToElapse):
+		if periodType == "days":
+			days = timeToElapse
+		elif periodType == "weeks":
+			days = 7 * timeToElapse
+		elif periodType == "months":
+			days = 30 * timeToElapse
 
-def severeImpact(reportedCases, factor):
-	currently_infected = reportedCases * 50
-	InfectionsByRequestedTime = currently_infected *(2**factor)
-	finaldata["severeImpact"]["currently_infected"] = currently_infected
-	finaldata["severeImpact"]["InfectionsByRequestedTime"] = InfectionsByRequestedTime
-	return
-	
-#Function to calculate and return the factor with all period type variations
-def factor_calculator(periodType, timeToElapse):
-	if periodType == "days":
-		days = timeToElapse
-	elif periodType == "weeks":
-		days = 7 * timeToElapse
-	elif periodType == "months":
-		days = 30 * timeToElapse
+		factor = days//3
 
-	factor = days//3
+		return factor
 
-	return factor
+	impact(reportedCases, factor)
+	severeImpact(reportedCases, factor)
+
+	return {
+
+	data:{data},
+	impact:{
+		currentlyInfected : currently_infected_impact,
+		InfectionsByRequestedTime : InfectionsByRequestedTime_impact
+		},
+	severeImpact:{
+		currentlyInfected :currently_infected_severeImpact
+		InfectionsByRequestedTime : InfectionsByRequestedTime_severeImpact
+	}
+
+	}
