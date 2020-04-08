2c9f4ba6e45e7bbe687288e656af2c6c0de7746c
The 254-kid <mgabbana8@yahoo.com>
2020-04-08 17:28:13 +0300

    Trial 5

--- src/estimator.py
+++ src/estimator.py
@@ -1,17 +1,38 @@
+def estimator(data):
+	newdata = data
+	periodType = newdata.periodType
+	timeToElapse = newdata.timeToElapse
+	reportedCases = newdata.reportedCases
+
+	factor = factor_calculator(periodType, timeToElapse)
+
+
+
+	i = impact(reportedCases, factor)
+	s = severeimpact(reportedCases, factor)
+
+		finaldata = {
+		data:newdata,
+		impact:{},
+		severeImpact:{}
+	}
+
+	data = finaldata
+	return data
 
 def impact(reportedCases, factor):
-	currently_infected_impact = reportedCases * 10
-	infectionsByRequestedTime_impact = currently_infected *(2**factor)
-	return [currently_infected_impact, infectionsByRequestedTime_impact]
-		
+	currently_infected = reportedCases * 10
+	InfectionsByRequestedTime = currently_infected *(2**factor)
+	return [currently_infected, InfectionsByRequestedTime]
+
+	
 
 def severeImpact(reportedCases, factor):
-	currently_infected_severe = reportedCases * 50
-	infectionsByRequestedTime_severe = currently_infected *(2**factor)
-	return [currently_infected_severe, infectionsByRequestedTime_severe]
+	currently_infected = reportedCases * 50
+	InfectionsByRequestedTime = currently_infected *(2**factor)
+	return [currently_infected, InfectionsByRequestedTime]
 	
-		
-	#Function to calculate and return the factor with all period type variations
+
 def factor_calculator(periodType, timeToElapse):
 	if periodType == "days":
 		days = timeToElapse
@@ -23,33 +44,3 @@ def factor_calculator(periodType, timeToElapse):
 	factor = days//3
 
 	return factor
-
-
-
-def estimator(data):
-	input = data
-	periodType = input.periodType
-	timeToElapse = input.timeToElapse
-	reportedCases = input.reportedCases
-
-	factor = factor_calculator(periodType, timeToElapse)
-
-	impact = impact(reportedCases, factor)
-	severe= severeImpact(reportedCases, factor)
-
-	newdata = {
-
-	data: input,
-	impact:{
-		currentlyInfected : impact[0],
-		InfectionsByRequestedTime : impact[1]
-		},
-	severeImpact:{
-		currentlyInfected :severe[0],
-		infectionsByRequestedTime : severe[1]
-	}
-
-	}
-	data = newdata
-
-	return data
