3428c2d1307c36a9571ae85850a78aa8da8c2562
The 254-kid <mgabbana8@yahoo.com>
2020-04-09 19:25:44 +0300

    Trial 6.2

--- src/estimator.py
+++ src/estimator.py
@@ -1,27 +1,55 @@
-def estimator(input_data):
+def estimator(data):
     impact = {}
     severeImpact = {}
+    reportedCases = data['reportedCases']
+    periodType = data['periodType']
+    timeToElapse = data['timeToElapse']
+    totalHospitalBeds = data['totalHospitalBeds']
+
     ###Normalizing the duration to just days
-    duration = duration_normaliser(input_data['periodType'],input_data['timeToElapse'])
+    duration = duration_to_days(periodType, timeToElapse)
+    factor = duration//3
     
-    ##CHALLENGE 1
-    ###calculating the estimated infections by the time duration
-    ##IMPACT
-    impact['currentlyInfected']=input_data['reportedCases'] * 10
-    impact['infectionsByRequestedTime'] = impact['currentlyInfected']*(pow(2,duration//3))
-    ##SEVEREIMPACT
-    severeImpact['currentlyInfected']=input_data['reportedCases']*50
-    severeImpact['infectionsByRequestedTime'] =severeImpact['currentlyInfected']*(pow(2,duration//3))
-    
-    
-    output_data = {'data':input_data, 'impact':impact, 'severeImpact': severeImpact}
-    
-    return output_data
+    #CHALLENGE 1
 
-def duration_normaliser(duration ,value):
-    if duration == "months":
-        value *= 30
-    elif duration == "weeks":
-        value *= 7
+    #For Impact
+    impact['currentlyInfected'] = reportedCases * 10
+    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * (pow(2,factor))
 
-    return value
+    #For Severe Impact
+    severeImpact['currentlyInfected'] = reportedCases * 50
+    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected']*(pow(2,factor))
+
+    #CHALLENGE 2
+
+    #For Impact
+    impact['severeCasesByRequestedTime'] = severeImpact['infectionsByRequestedTime'] * 0.15
+    impact['hospitalBedsByRequestedTime'] = beds_available(totalHospitalBeds, impact['severeCasesByRequestedTime'])
+
+
+    #For Severe Impact
+    impact['severeCasesByRequestedTime'] = severeImpact['infectionsByRequestedTime'] * 0.15
+    impact['hospitalBedsByRequestedTime'] = beds_available(totalHospitalBeds, impact['severeCasesByRequestedTime'])
+
+
+
+
+
+    output = {'data':input_data, 'impact':impact, 'severeImpact': severeImpact}
+
+    return output
+
+def duration_to_days(durationType ,time):
+    if durationType == "months":
+        time *= 30
+    elif durationType == "weeks":
+        time *= 7
+
+    return time
+
+#calculating the number of beds available
+def beds_available(totalbeds,severecases):
+    available_beds = (0.35 * totalbeds) - severecases
+    return available_beds
+
+
